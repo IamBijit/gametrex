4adbbaf9495e0b263ece3bc573567f458b907aec
IamBijit <54630900+IamBijit@users.noreply.github.com>
2020-08-02 15:11:22 +0530

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+
+module.exports.run = async (bot, message, args) => {
+  if(message.author.bot) return;
+  
+  let prefix = config.prefix;
+  
+  if(!message.content.startsWith(prefix)) return;
+  
+  const m = await message.channel.send("Hold on .....")
+  
+  let pong = new Discord.MessageEmbed()
+  .setTitle("🏓 Pong!")
+  .setColor('RANDOM')
+  .setTimestamp()
+  .addField("Latency", `${m.createdTimestamp - message.createdTimestamp}ms`, true)
+  .addField("API Latency", `${Math.round(bot.ws.ping)}ms`, true)
+  .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL());
+
+  m.edit(pong)
+}
+
+module.exports.help = {
+  name: "ping",
+  type:"fun",
+  usage:"`Ping`",
+  about:"Ping pong command ...xD",
+  aliases: [""],
+
+}
